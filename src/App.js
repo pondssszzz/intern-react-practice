3d5114ad99e4c7d0a84a072d06c021dbd382c98d
Pond <pond34021@hotmail.com>
2021-05-25 00:33:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
+    <div className="grid-container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        Reserved
+      </footer>
     </div>
   );
 }
